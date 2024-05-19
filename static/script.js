233f58ba912142776022ab764a47590583c97326
Prakkks <tprakrity@gmail.com>
2024-05-19 18:05:46 +0545

    Added functionality to add picture

--- static/script.js
+++ static/script.js
@@ -1,1 +1,29 @@
-alert("hello")
+// alert("hello")
+const dropArea = document.getElementById("drop-area");
+const inputFile = document.getElementById("input-file");
+const imageView = document.getElementById("img-view");
+
+
+
+inputFile.addEventListener("change",uploadImage);
+
+function uploadImage(){
+    // inputFile.files[0];
+    let imgLink = URL.createObjectURL(inputFile.files[0]); //gives url of image
+    imageView.style.backgroundImage = `url(${imgLink})`;
+    imageView.textContent="";
+    imageView.style.border= 0;
+    // inputFile.hidden = false;
+}
+
+dropArea.addEventListener("dragover", function(e){
+    e.preventDefault();
+
+})
+
+dropArea.addEventListener("drop", function(e){
+    e.preventDefault();
+    inputFile.files = e.dataTransfer.files;
+    uploadImage();
+
+})
