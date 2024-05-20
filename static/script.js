e430e95f3b5eb2350ece75986981d2bca168a689
Prakkks <tprakrity@gmail.com>
2024-05-20 19:26:18 +0545

    Modified UI

--- static/script.js
+++ static/script.js
@@ -1,7 +1,9 @@
 // alert("hello")
 const dropArea = document.getElementById("drop-area");
 const inputFile = document.getElementById("input-file");
+const submit = document.getElementById("submit");
 const imageView = document.getElementById("img-view");
+const cardContainer = document.getElementById("image-info");
 // const home = document.getElementById("droparea");
 
 
@@ -10,12 +12,52 @@ inputFile.addEventListener("change",uploadImage);
 
 function uploadImage(){
     // inputFile.files[0];
-    // dropArea.style.width ='50%' ;
+    file = inputFile.files[0];
     let imgLink = URL.createObjectURL(inputFile.files[0]); //gives url of image
-    imageView.style.backgroundImage = `url(${imgLink})`;
-    imageView.textContent="";
-    imageView.style.border= 0;
-    // inputFile.hidden = false;
+     fileName = file.name;
+     fileSize = (file.size / (1024 * 1024)).toFixed(2); // Size in MB
+    // imageView.style.backgroundImage = `url(${imgLink})`;
+    // var a = `url(${imgLink})`;
+    // imageView.textContent="";
+    // imageView.style.border= 0;
+    createnewcard(imgLink,fileName,fileSize);
+    
+    cardContainer.hidden=false;
+    submit.hidden = false;
+}
+
+function createnewcard(a,b,c)
+{
+    const newCard = document.createElement('div');
+    newCard.innerHTML = ` 
+    <div class="vertical-card">
+      <div class="image-show">
+        <img  src="${a}"  id="uploaded-img"  > 
+        <span>
+           ${b} 
+           <br>
+          
+        </span>
+      </div>
+    <button type="button" class="btn-close"  aria-label="Close"></button>
+    </div> `;
+    // cardContainer.appendChild(newCard);
+    cardContainer.insertBefore(newCard, cardContainer.firstChild);
+    const closeButton = newCard.querySelector('.btn-close');
+    closeButton.addEventListener('click', deleteCard);
+}
+
+
+function deleteCard(event) {
+    const card = event.target.closest('.vertical-card');
+    if (card) {
+        card.remove();
+    }
+    if (document.querySelectorAll('.vertical-card').length === 0) {
+        submit.hidden = true; // Hide the submit button
+    cardContainer.hidden=true;
+
+    }
 }
 
 dropArea.addEventListener("dragover", function(e){
